8e469e45c8712f99195117c7bfa984d0b976ba56
shiyf <shiyf@lenovoconnect.com>
2019-08-15 16:36:50 +0800

    提交git过滤文件

--- dubbo-user/api/src/main/java/com/shiyifan/user/abs/AbstractResponse.java
+++ dubbo-user/api/src/main/java/com/shiyifan/user/abs/AbstractResponse.java
@@ -7,11 +7,31 @@ public abstract class AbstractResponse  implements Serializable {
 	/**
 	 * 返回码（请求）
 	 */
-	private String code;
+	public String code;
 
 	private String msg;
 
+	public String getCode() {
+		return code;
+	}
 
+	public void setCode(String code) {
+		this.code = code;
+	}
 
+	public String getMsg() {
+		return msg;
+	}
 
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	@Override
+	public String toString() {
+		return "AbstractResponse{" +
+				"code='" + code + '\'' +
+				", msg='" + msg + '\'' +
+				'}';
+	}
 }
--- dubbo-user/provider/src/main/java/com/shiyifan/services/UserCoreService.java
+++ dubbo-user/provider/src/main/java/com/shiyifan/services/UserCoreService.java
@@ -1,19 +1,44 @@
 package com.shiyifan.services;
 
+import com.shiyifan.dao.mapper.UserMapper;
+import com.shiyifan.dao.po.User;
 import com.shiyifan.user.IUserCoreService;
+import com.shiyifan.user.constants.ResponseCodeEnum;
 import com.shiyifan.user.dto.UserLoginRequest;
 import com.shiyifan.user.dto.UserLoginResponse;
 import com.shiyifan.user.dto.UserRegisterRequest;
 import com.shiyifan.user.dto.UserRegisterResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("UserCoreService")
 public class UserCoreService implements IUserCoreService {
 
+	private static final Logger LOGGER=LoggerFactory.getLogger(UserCoreService.class);
+
+	@Autowired
+	private UserMapper userMapper;
 
 	@Override
 	public UserLoginResponse login(UserLoginRequest userLoginRequest) {
-		return null;
+		LOGGER.info("begin login request "+userLoginRequest.toString());
+		UserLoginResponse userLoginResponse = new UserLoginResponse();
+		User user = userMapper.getUserByUserName(userLoginRequest.getUsername());
+		if(user==null || !userLoginRequest.getPassword().equals(user.getPassword())){
+			userLoginResponse.setCode(ResponseCodeEnum.USER_OR_PASSWORD_ERROR.getCode());
+			userLoginResponse.setMsg(ResponseCodeEnum.USER_OR_PASSWORD_ERROR.getMsg());
+			return userLoginResponse;
+		}
+		//设置用户状态
+		userLoginResponse.setAvatar(user.getAvatar());
+		userLoginResponse.setMobile(user.getMobile());
+		userLoginResponse.setSex(user.getSex());
+		userLoginResponse.setRealName(user.getRealname());
+		userLoginResponse.setCode(ResponseCodeEnum.SYS_SUCCESS.getCode());
+		userLoginResponse.setMsg(ResponseCodeEnum.SYS_SUCCESS.getMsg());
+		return userLoginResponse;
 	}
 
 	@Override
